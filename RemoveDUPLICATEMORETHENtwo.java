86465ec52602787674de216339d2c021ef426d5c
Amitabha Ghosh <amitabhag17@gmail.com>
2026-08-12 08:52:11 +0530

    Add method to remove duplicates from array
    Implement a method to remove duplicates that appear more than twice from a sorted array.

---
+++ RemoveDUPLICATEMORETHENtwo.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int k = nums.length;
+
+      
+
+        for(int i = 0; i < k - 2; i++) {
+
+            if(nums[i] == nums[i + 1]&&nums[i]==nums[i+2]) {
+
+                for(int j = i + 2; j < k - 1; j++) {
+                    nums[j] = nums[j + 1];
+                }
+
+                k--;
+                i--;
+            }
+        }
+
+        return k;
+    }
+}
