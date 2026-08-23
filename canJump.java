880c36a06e1b8db36e89d9fddd0257eb0e20efd5
Amitabha Ghosh <amitabhag17@gmail.com>
2026-08-23 22:50:34 +0530

    Implement canJump method to check jump ability

---
+++ canJump.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int m=0;
+
+        for(int i=0;i<nums.length;i++){
+            if(i>m){
+                return false;
+            }
+            m=Math.max(m,i+nums[i]);
+
+            if(m>=nums.length){
+                return true;
+            }
+        }return true;
+    }
+}
